6dfa5065ce5e6ae6b75d35550d5c4c60cc9b73b5
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 17:57:23 +0530

    Update iam-policy.tf

--- iam-policy.tf
+++ iam-policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy" "iam-policy" {
   name   = "AWS-ECS-Execution-Role"
   role   = aws_iam_role.iam-role.id
-  policy = file("./iam-policy.json")
+  policy = file("iam-policy.json")
 }
