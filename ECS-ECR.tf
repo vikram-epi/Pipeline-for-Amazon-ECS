bde9b883bcbec373bbc91464237f89d5eede2c02
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 10:44:37 +0530

    Update ECS-ECR.tf

--- ECS-ECR.tf
+++ ECS-ECR.tf
@@ -10,7 +10,7 @@ resource "aws_ecs_task_definition" "task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = 256
   memory                   = 512
-  execution_role_arn       = "${data.aws_iam_role.ecs-task.arn}"
+  execution_role_arn       = "${aws_iam_role.iam-role.arn}"
 
   container_definitions = jsonencode([
     {
