cce213c6e5c18dfaf4ccd5d585c737a620e5f610
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 17:11:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,17 @@
 terraform {
-  backend "s3" {
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 4.18.0"
     }
-}
+  }
+
+  backend "s3" {
+    bucket         	   = "mycomponents-tfstate"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+  }
 }
 
 provider "aws" {
