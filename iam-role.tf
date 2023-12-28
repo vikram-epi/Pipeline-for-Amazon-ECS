a930577e9bc36c436d6203134fe8632012f0da4a
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 17:56:58 +0530

    Update iam-role.tf

--- iam-role.tf
+++ iam-role.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_role" "iam-role" {
   name               = "iam-role-ECS-Execution"
-  assume_role_policy = file("./iam-role.json")
+  assume_role_policy = file("iam-role.json")
 }
