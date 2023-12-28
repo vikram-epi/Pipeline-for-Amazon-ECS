0b373cfc8095bc39bf4563fde5ce7f58765d269d
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 11:30:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
 terraform {
-  backend "s3" {
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 4.18.0"
     }
-}
-}
+  }
 
+  backend "s3" {
+    bucket         	   = "mycomponents-tfstate1"
+    key              	   = "mycomponents-tfstate1/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+  }
+}
 provider "aws" {
-  region = "eu-west-1"
+  region  = "us-east-1"
 }
 
 # ECS CLUSTER
