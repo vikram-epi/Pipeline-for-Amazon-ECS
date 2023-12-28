2ced676b0525c5511124c09286f254b8354ab5ae
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 17:31:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,10 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         	   = "mycomponents-tfstate"
-    key              	   = "state/terraform.tfstate"
-    region         	   = "us-east-1"
-    encrypt        	   = true
+    
   }
 }
 
